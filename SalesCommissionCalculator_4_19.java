07ffbd271d091d0e016761415245d6978be5b65b
InaStici <33605290+InaStici@users.noreply.github.com>
2017-11-19 22:20:25 +0200

    Update and rename SalesCommissioCalculator_4_19.java to SalesCommissionCalculator_4_19.java

--- SalesCommissionCalculator_4_19.java
+++ SalesCommissionCalculator_4_19.java
@@ -1,6 +1,6 @@
 package exercises;
 import java.util.Scanner;
-public class SalesCommissioCalculator_4_19 {
+public class SalesCommissionCalculator_4_19 {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
@@ -22,19 +22,19 @@ public class SalesCommissioCalculator_4_19 {
 		//items = 4;
 		switch (items) {
 		case 1:
-			 earning1 = 200 + v1 * 0.09;
+			 earning1 += v1;
 			System.out.printf("Earning for item 1 is: %.2f%n", earning1);
 			break;
 		case 2:
-			 earning2 = 200 + v2 * 0.09;
+			 earning2 += v2;
 			System.out.printf("Earning for item 2 is: %.2f%n", earning2);
 			break;
 		case 3:
-			 earning3 = 200 + v3 * 0.09;
+			 earning3 += v3;
 			System.out.printf("Earning for item 3 is: %.2f%n", earning3);
 			break;
 		case 4:
-			 earning4 = 200 + v4 * 0.09;
+			 earning4 += v4;
 			System.out.printf("Earning for item 4 is: %.2f%n", earning4);
 			break;
 		default:
@@ -42,14 +42,16 @@ public class SalesCommissioCalculator_4_19 {
 			break;
 			
 			}
-		totalEarning = earning1 + earning2 + earning3 + earning4;
-		
+		totalEarning = earning1 + earning2 + earning3 + earning4 ;
+		System.out.printf("Total earning is %.2f%n", totalEarning);	
 		System.out.print("Enter items sold or 0 to quit: ");
 		items = input.nextInt();
-		
 	}
 		
-		System.out.printf("Salesperson's earning is: %.2f%n", totalEarning);
+		double salary = 200 + totalEarning * 0.09;	
+		
+		
+		System.out.printf("Salesperson's earning is: %.2f%n", salary);
 	}
 
 }
