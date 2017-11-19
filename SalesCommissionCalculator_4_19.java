350e5155a98b79fbcdb9479ee65f044ba384c16a
InaStici <33605290+InaStici@users.noreply.github.com>
2017-11-19 22:37:44 +0200

    Update SalesCommissionCalculator_4_19.java

--- SalesCommissionCalculator_4_19.java
+++ SalesCommissionCalculator_4_19.java
@@ -5,10 +5,6 @@ public class SalesCommissionCalculator_4_19 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		double v1 = 239.99;
-		double v2 = 129.75;
-		double v3 = 99.95;
-		double v4 = 350.89;
 		double totalEarning = 0;
 		double earning1 = 0;
 		double earning2 = 0;
@@ -22,19 +18,19 @@ public class SalesCommissionCalculator_4_19 {
 		//items = 4;
 		switch (items) {
 		case 1:
-			 earning1 += v1;
+			 earning1 = 239.99;
 			System.out.printf("Earning for item 1 is: %.2f%n", earning1);
 			break;
 		case 2:
-			 earning2 += v2;
+			 earning2 = 129.75;
 			System.out.printf("Earning for item 2 is: %.2f%n", earning2);
 			break;
 		case 3:
-			 earning3 += v3;
+			 earning3 = 99.95;
 			System.out.printf("Earning for item 3 is: %.2f%n", earning3);
 			break;
 		case 4:
-			 earning4 += v4;
+			 earning4 = 350.89;
 			System.out.printf("Earning for item 4 is: %.2f%n", earning4);
 			break;
 		default:
