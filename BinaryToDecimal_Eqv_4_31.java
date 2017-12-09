6c214c15cff0a5dbc0bade566c3e54cf81aabfc9
InaStici <33605290+InaStici@users.noreply.github.com>
2017-12-09 19:10:32 +0200

    Add files via upload

---
+++ BinaryToDecimal_Eqv_4_31.java
@@ -0,0 +1,27 @@
+package exercises;
+import java.util.Scanner;
+public class BinaryToDecimal_Eqv_4_31 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a binary number:");
+        int binary = input.nextInt();
+        
+        int decimal=0;
+        int p=0;
+        
+        while(binary != 0)
+        {
+            decimal+=((binary%10)*Math.pow(2,p));
+            binary = binary/10;
+            p++;
+        }
+        
+        System.out.println(decimal);
+		
+
+	}
+
+}
