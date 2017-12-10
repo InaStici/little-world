1b5ddba79399762f1c1c23bb36326db02fb61224
InaStici <33605290+InaStici@users.noreply.github.com>
2017-12-10 20:51:50 +0200

    Add files via upload

---
+++ FindTheSmallestValue_5_11.java
@@ -0,0 +1,39 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class FindTheSmallestValue_5_11 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int counter = 1;
+		int number;
+		int smallest = Integer.MAX_VALUE;
+		int nrVal;
+		
+		System.out.print("Enter number of values: ");
+		nrVal = input.nextInt();
+		
+		System.out.print("Enter " +nrVal+ " numbers: ");
+		number = input.nextInt();
+		
+		while (counter < nrVal) {
+			
+			number = input.nextInt();
+			
+			if (number < smallest) {
+				smallest = number;
+			} 
+			
+			counter +=1;
+		}
+		System.out.println();
+		
+		System.out.println("You entered " + counter + "numbers");
+		System.out.println("The smallest number is: " + smallest);
+		
+
+	}
+
+}
