39479dec13a1f44f7376d6023e6e9646994776f9
InaStici <33605290+InaStici@users.noreply.github.com>
2017-12-24 14:40:42 +0200

    Add files via upload

---
+++ ParkingCharges_6_8.java
@@ -0,0 +1,29 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class ParkingCharges_6_8 {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter parking hours: ");
+		double hours = input.nextDouble();
+		double charge = chargePerStay(hours);
+		System.out.println("The parking charge is " + charge);
+	}
+	
+	public static double chargePerStay(double x) {
+		double fee = 0;
+		
+		if (x <= 3) {
+			fee = x * 2.00;
+			}
+		
+		if (x > 3) {
+			fee = 6.00 + (x - 3) * 0.50;
+		}
+		
+		return fee;
+		}
+}
